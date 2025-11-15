e151edd0ba6f0d100bc49a8703a66b4a032017ea
zyxiong <zyxiong@MacBook-Pro-2.local>
2025-11-15 11:57:30 -0800

    amend for lec1-6 code

--- tutorial_code/lec3/re_linear_regression.py
+++
@@ -1,9 +0,0 @@
-from autograd import grad
-import matplotlib.pyplot as plt
-import numpy as np
-
-def model(x, w):
-    return w[0] + np.dot(x.T, w[1:])
-
-def mse(x, y):
-    return sum() / x.shape(0)
